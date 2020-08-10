ccabf6db4355db038e84389fb7b4b01db31e0695
schnuppernWGC <marc33@gmx.ch>
2020-08-10 13:24:30 +0200

    form

---
+++ WebApplication/1_StaticWebHosting/website/js/contact.js
@@ -0,0 +1,33 @@
+function sendContactRequest(event){
+    //var values = $(name-input).val();
+    event.preventDefault();
+
+    $.ajax({
+        method: 'POST',
+        url: _config.api.invokeUrl + '/contact',
+        headers: {
+        },
+        data: JSON.stringify({
+            ContactData: {
+                name: "Marc",
+                tel: "079 521 75 99",
+                email: "marc33@gmx.ch",
+                request: "Dies ist eine Kontaktanfrage"
+            }
+        }),
+        contentType: 'application/json',
+        success: completeRequest,
+        error: function ajaxError(jqXHR, textStatus, errorThrown) {
+            console.error('Error sending form: ', textStatus, ', Details: ', errorThrown);
+            console.error('Response: ', jqXHR.responseText);
+            alert('An error occured when sending the form:\n' + jqXHR.responseText);
+        }
+    });
+}
+
+
+$(function onDocReady() {
+
+    $('#contact-form').click(sendContactRequest);
+    
+});
